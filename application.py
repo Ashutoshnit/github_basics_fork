bfe910b76b77a29e046f71b890907cfb9b8c6c93
Ashutoshnit <104322745+Ashutoshnit@users.noreply.github.com>
2024-02-10 12:44:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
-"Ashutosh Github basics"
+"Ashutosh Github basics" "Commit"
 @application.route("/")
 def root():
     return render_template("index.html")
